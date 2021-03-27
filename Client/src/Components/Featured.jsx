42976e475afb9abccb45d736b87d397c847f1877
francizjamez <francizjamez@gmail.com>
2021-03-27 15:58:09 +0800

    added rating

--- Client/src/Components/Featured.jsx
+++ Client/src/Components/Featured.jsx
@@ -1,11 +1,12 @@
 import "../Styles/Style-Featured.css";
+import Rating from "./Rating.jsx";
 
 const Featured = () => {
-    return (
-        <section id="container">
-            
-        </section>
-    )
-}
+  return (
+    <section id="container">
+      <Rating rating="101" />
+    </section>
+  );
+};
 
-export default Featured
+export default Featured;
---
+++ Client/src/Components/Rating.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Star from "../Images/icons8-star-filled-96.png";
+
+const Rating = ({ rating, size }) => {
+  let starSize = size;
+  let starStyle = {
+    width: starSize + "px",
+    height: starSize + "px",
+  };
+  const createRating = (n) => {
+    let ratings = [];
+
+    let star = <img src={Star} style={starStyle} />;
+
+    for (let i = 0; i < n; i++) {
+      ratings.push(star);
+    }
+
+    return ratings;
+  };
+
+  let ratings = createRating(rating);
+
+  return <div>{ratings.map((val) => val)}</div>;
+};
+
+Rating.defaultProps = {
+  size: "30",
+};
+
+export default Rating;
